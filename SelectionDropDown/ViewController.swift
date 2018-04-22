25660352eb99cf0226b1a794b0a7ac624f2025ee
madoffox <minsk315a@gmail.com>
2018-04-22 22:40:37 +0300

    Update ViewController.swift

--- SelectionDropDown/ViewController.swift
+++ SelectionDropDown/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.magenta
+        //self.view.backgroundColor = UIColor.magenta
         
         selectionDropDownView = SelectionDropDownView(frame: CGRect(x: 10, y: 40, width: self.view.frame.width-20, height: self.view.frame.height/3 ))
         selectionDropDownView.cornerRadius = 12
